f20dc0189f8852a8397be9b9b2da37731f57a7f5
Oskar Grunning <oskargrunning@gmail.com>
2021-04-08 07:46:18 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BackgroundCloud, Title } from "./parts";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <main className="App">
+      <BackgroundCloud />
+      <Title />
+    </main>
+  );
+}
---
+++ src/parts/index.ts
@@ -0,0 +1,2 @@
+export * from "./BackgroundCloud";
+export * from "./Title";
